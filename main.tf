9db60b7040612beb29fb95e56ef411d5bf0afb59
Ashwin Naik <ashwinknaik@google.com>
2022-06-13 14:31:02 +0530

    2

--- main.tf
+++ main.tf
@@ -8,6 +8,12 @@ variable "project_id" {
 }
 
 
+provider "google" {
+  user_project_override = true
+  access_token          = var.access_token
+  project               = var.project_id
+}
+
 resource "google_composer_environment" "test" {
     project =  var.project_id
   name   = "example-composer-env"
